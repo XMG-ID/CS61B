e1e37d210a5bbe615374f777ad0ea8aa42b3c31e
241880421 <241880421@smail.nju.edu.cn>
2025-04-29 09:34:54 +0800

    complete the guitar

--- proj1/gh2/GuitarHeroLite.java
+++ proj1/gh2/GuitarHeroLite.java
@@ -1,4 +1,7 @@
 package gh2;
+
+import deque.ArrayDeque;
+import deque.Deque;
 import edu.princeton.cs.algs4.StdAudio;
 import edu.princeton.cs.algs4.StdDraw;
 
@@ -10,31 +13,39 @@ public class GuitarHeroLite {
     public static final double CONCERT_C = CONCERT_A * Math.pow(2, 3.0 / 12.0);
 
     public static void main(String[] args) {
-        /* create two guitar strings, for concert A and C */
-        GuitarString stringA = new GuitarString(CONCERT_A);
-        GuitarString stringC = new GuitarString(CONCERT_C);
+        /*Create all guitar strings, store in the guitarStringDeque*/
+        String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+        Deque<GuitarString> guitarStringDeque = new ArrayDeque<>();
+        for (int i = 0; i < keyboard.length(); i++) {
+            double concert = CONCERT_A * Math.pow(2, (i - 24) / 12.0);
+            guitarStringDeque.addLast(new GuitarString(concert));
+        }
 
         while (true) {
 
             /* check if the user has typed a key; if so, process it */
             if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
-                if (key == 'a') {
-                    stringA.pluck();
-                } else if (key == 'c') {
-                    stringC.pluck();
+                int index = keyboard.indexOf(key);
+                if (index == -1) {
+                    continue;
                 }
+                guitarStringDeque.get(index).pluck();
             }
 
             /* compute the superposition of samples */
-            double sample = stringA.sample() + stringC.sample();
+            double sample = 0.0;
+            for(int i=0;i<guitarStringDeque.size();i++){
+                sample+=guitarStringDeque.get(i).sample();
+            }
 
             /* play the sample on standard audio */
             StdAudio.play(sample);
 
             /* advance the simulation of each guitar string by one step */
-            stringA.tic();
-            stringC.tic();
+            for(int i=0;i<guitarStringDeque.size();i++){
+                guitarStringDeque.get(i).tic();
+            }
         }
     }
 }
